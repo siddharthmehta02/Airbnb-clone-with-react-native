8011ab82f7ea258f5a89e5bbedc8b9a042ec0bf0
siddharthmehta02 <sidmehta0201@gmail.com>
2020-12-28 13:56:04 +0530

    Top Nav bars UI

---
+++ src/navigations/SearchResultsTabNavigator.js
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import React from 'react'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import SearchResultsScreen from '../screens/SearchResults'
+
+const Tab = createMaterialTopTabNavigator()
+const SearchResultsTabNavigator = () => {
+    return (
+        <Tab.Navigator tabBarOptions={{
+            activeTintColor: '#f15454',
+            indicatorStyle: {
+                backgroundColor: '#f15454'
+            }
+        }}>
+            <Tab.Screen
+                name={"list"}
+                component={SearchResultsScreen}
+            />
+            <Tab.Screen
+                name={"map"}
+                component={SearchResultsScreen}
+            />
+        </Tab.Navigator>
+    )
+}
+export default SearchResultsTabNavigator
